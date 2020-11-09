af5a28a2174fb04abb3c74987cd4fa4d6b9692f7
Armin Parnia <Paarmin>
2020-11-09 13:45:20 -0500

    cc

--- JavaScript_Fundamentals/forLoop.js
+++ JavaScript_Fundamentals/forLoop.js
@@ -1,9 +1,15 @@
-for (let i = 100 ; i < 201 ; i++){
-  if ( i % 3 === 0 && i % 4 === 0){
-    console.log("LoopyLighthouse");
-  } else if ( i % 3 === 0 ){
-    console.log("Loopy");
-  } else if ( i % 4 === 0 ){
-    console.log("Lighthouse");
-  }
+// for (let i = 100 ; i < 201 ; i++){
+//   if ( i % 3 === 0 && i % 4 === 0){
+//     console.log("LoopyLighthouse" + i );
+//   } else if ( i % 3 === 0 + i ){
+//     console.log("Loopy");
+//   } else if ( i % 4 === 0 ){
+//     console.log("Lighthouse" + i );
+//   }
+// }
+
+function test(){
+  return 1;
+  return 2;
 }
+test();
---
+++ JavaScript_Fundamentals/odd.js
@@ -0,0 +1,8 @@
+function isOdd (number){
+  if (number % 3 === 0){
+    return true;
+  } else{ 
+    return false;
+  }
+}
+console.log ( "32212 is odd: " + isOdd(32212));
