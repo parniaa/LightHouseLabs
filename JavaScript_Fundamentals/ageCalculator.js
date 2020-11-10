80c09f0476f670309bf80b74b0fce7e68601265d
Armin Parnia <Paarmin>
2020-11-10 12:08:48 -0500

    11-10

---
+++ JavaScript_Fundamentals/ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator (name, yB, Cy){
+  let age = Cy - yB;
+  return( name + " is " + age + " years old." )
+  
+}
+console.log(ageCalculator("armin", 2004, 2016));
---
+++ JavaScript_Fundamentals/finalPosition.js
@@ -0,0 +1,19 @@
+const moves = ['south', 'south', 'south', 'south', 'south', 'north','south']  
+
+function finalPosition(input){
+  let result = [0,0];
+  for (const move of moves){
+    if (move === "north" ){
+      result[1]++;
+    } else if (move === "south" ){
+      result[1]--;
+    } else if (move === "east" ){
+      result[0]++;
+    } else if (move === "west"){
+      result[0]--;
+    }
+  }
+  return result;
+}
+var t1 = finalPosition(moves);
+console.log(t1);
