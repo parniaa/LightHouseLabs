2bfb30db02e785a97f7af78291fef29afa747d57
Armin Parnia <Paarmin>
2020-11-20 00:58:01 -0500

    ok

--- JavaScript_Fundamentals/finalPosition.js
+++ JavaScript_Fundamentals/finalPosition.js
@@ -1,22 +1,22 @@
-const moves = [  'south',
-'south',
-'south',
-'south',
-'south',
-'north',
+const moves = ['east',
+'east',
+'east',
+'east',
+'west',
+'west',
 'south'];  
 
 function finalPosition(input){
   let result = [0,0];
   for (const move of moves){
     if (move === "north" ){
-      result[1]++;
-    } else if (move === "south" ){
-      result[1]--;
-    } else if (move === "east" ){
       result[0]++;
-    } else if (move === "west"){
+    } else if (move === "south" ){
       result[0]--;
+    } else if (move === "east" ){
+      result[1]++;
+    } else if (move === "west"){
+      result[1]--;
     }
   }
   return result;
