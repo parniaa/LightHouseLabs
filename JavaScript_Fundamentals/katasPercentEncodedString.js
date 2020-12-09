ed965b96674b9e96b4e6fdab56ae263e615730d3
Armin Parnia <Paarmin>
2020-12-09 13:02:08 -0500

    all

---
+++ JavaScript_Fundamentals/katasPercentEncodedString.js
@@ -0,0 +1,12 @@
+const urlEncode = function(text) {
+  let results = text.split('');
+  for (const resut of results) {
+    if ( result === " ") {
+      resut
+    }
+  }
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
---
+++ JavaScript_Fundamentals/test.js
@@ -0,0 +1,9 @@
+n = "1122";
+function test(n) {
+  let g = n.split("");
+  return g;
+  for (let i = 0; index < digits.length; index++) {
+
+    
+  }
+}
