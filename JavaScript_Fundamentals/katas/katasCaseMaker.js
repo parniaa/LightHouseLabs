4b808595b1a82e736f8d578e967ea5ee2e7667a3
Armin Parnia <Paarmin>
2020-12-21 23:24:20 -0500

    final

--- JavaScript_Fundamentals/katas/katasCaseMaker.js
+++ JavaScript_Fundamentals/katas/katasCaseMaker.js
@@ -1,21 +1,14 @@
 const camelCase = function(input) {
-  let dataArrays = input.split("");
-  // console.log(dataArrays);
-  let finalResult =[];
+  let dataArrays = input.split('');
   for (let i = 0; i < dataArrays.length; i++) {
     let data = dataArrays[i];
     if (data ===" ") {
-      
       dataArrays[i+1] = dataArrays[i+1].toUpperCase();
-      dataArrays.splice(i, 1);
-      
+      dataArrays.splice(i, 1);  
     }
   }
-
-    console.log(dataArrays);
-  
+  return dataArrays.join('');  
 };
-
 console.log(camelCase("this is a string"));
-// console.log(camelCase("loopy lighthouse"));
-// console.log(camelCase("supercalifragalisticexpialidocious"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
