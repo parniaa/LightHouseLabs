2a6de53e95f546e3677b3e16eb9e34eae9edeeff
paarmin <parniaa@gmail.com>
2021-01-05 17:02:00 -0500

    kk

--- w1/d2-focal/pig_latin_translator.js
+++ w1/d2-focal/pig_latin_translator.js
@@ -1,12 +1,12 @@
 var originalWords = process.argv.slice(2);
 var pigLatinWords = [];
 
-for (var i = 1; i < originalWords.length; i++) {
+for (var i = 0; i < originalWords.length; i++) {
   pigLatinWords.push(translateToPigLatin(originalWords[i]));
 }
 
 console.log(pigLatinWords.join(' '));
 
 function translateToPigLatin(word) {
-  return word.slice(2, word.length) + word[0] + "ay";
+  return word.slice(1, word.length) + word[0] + "ay";
 }
