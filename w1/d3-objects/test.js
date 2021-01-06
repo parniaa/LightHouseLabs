ca3cad9ebac9b693e725525ee19c74cb2b5988da
paarmin <parniaa@gmail.com>
2021-01-06 13:27:01 -0500

    oo

--- w1/d3-objects/test.js
+++ w1/d3-objects/test.js
@@ -10,3 +10,12 @@ const person = {
 person["pNumber"] = [2222, 22221,4422];
 console.log(person.pNumber instanceof Array);
 console.log(Object.length(person));
+for (const iterator of object) {
+  
+}
+for (const key in object) {
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+    
+  }
+}
