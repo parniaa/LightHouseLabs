189d29c49714f2e99c47cb20a472c63ea822ab40
paarmin <parniaa@gmail.com>
2021-01-06 16:44:14 -0500

    Co-authored-by: Luc Borris <Borrisson@users.noreply.github.com>

---
+++ w1/d3-objects/raisin.js
@@ -0,0 +1,17 @@
+const raisinAlarm = function (cookie) {
+  // Put your solution here
+  let flag = true;
+  for (const ingredient of cookie) {
+    if (ingredient === "🍇") { flag = false; }
+  }
+  if (flag) {
+    return "All good";
+  }
+  else {
+    return "Raising Alert!";
+  }
+
+};
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
