583d3a100bc8c25073940b44d1bc136e2f13c769
paarmin <parniaa@gmail.com>
2021-01-07 14:51:56 -0500

    map utilized

--- w1/d4-funcs/map_builtin/mapTest.js
+++ w1/d4-funcs/map_builtin/mapTest.js
@@ -2,3 +2,6 @@ const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discove
 
 // write code here to use map
 // It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
+
+const map1 = lighthouses.map(x => x.length);
+console.log(map1);
