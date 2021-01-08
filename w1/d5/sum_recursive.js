de4e75e500fc33fe261c0cf7ef59681c0a3fd0c2
paarmin <parniaa@gmail.com>
2021-01-08 17:14:28 -0500

    recursive

--- w1/d5/sum_recursive.js
+++ w1/d5/sum_recursive.js
@@ -1,9 +1,9 @@
 const recursiveSum = function (input) {
-  if (input === 10) {
+  if (input === 0) {
     return 0;
-    console.log("g");
+    
   }
-  return input + recursiveSum(input + 1);
-  console.log("1");
+  return input + recursiveSum(input - 1);
+  
 };
 console.log(recursiveSum(4));
