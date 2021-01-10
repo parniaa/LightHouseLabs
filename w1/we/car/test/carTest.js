63d767de5a74ee4f177d80849e076520df51cd55
paarmin <parniaa@gmail.com>
2021-01-10 10:35:38 -0500

    npm added

---
+++ w1/we/car/test/carTest.js
@@ -0,0 +1,12 @@
+const chai = require('chai'); // 1
+const assert = chai.assert;
+
+const shouldBuyCar = require('../shouldBuyCar.js'); // 2
+
+describe("#shouldBuyCar()", function() { // 3
+
+  it("should return false if it's a hatchback", function() { // 4
+
+  });
+
+});
