a68f113fd816ca14e41079378d38f36d6d9f2a77
paarmin <parniaa@gmail.com>
2021-01-15 12:26:21 -0500

    ok

---
+++ w2/d5/testingExam.js
@@ -0,0 +1,33 @@
+// console.log(!Number.isNaN("22"));
+// let a = "4";
+// if (!Number.isNaN(a)) {
+//   console.log(NaN);
+// }
+// let a = {a: 1, b: 2, c: 3};
+// let b = {here: 3, is: 2, a: 1, silly: 0, example: -1};
+// let key = 'g';
+
+// // let t1 = a.hasOwnProperty(key);
+// // console.log(t1);
+
+
+
+// const keyMatcher = function(firstObj, secondObj, key) {
+
+//   if (firstObj.hasOwnProperty(key) && secondObj.hasOwnProperty(key)) {
+//     if (firstObj[key] === secondObj[key]) {
+//       return true;
+//     }
+//   }
+
+// };
+
+
+// let tx = keyMatcher(a, b, key);
+// console.log(tx);
+// for (const key in obj) {
+//   if (key === a){
+//     console.log(obj);
+//     }
+// }
+
