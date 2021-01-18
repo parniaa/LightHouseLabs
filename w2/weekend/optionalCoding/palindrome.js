b30526bb38781f195756a7cbaaca45d98815b387
paarmin <parniaa@gmail.com>
2021-01-17 23:21:28 -0500

    ok palindrome

--- w2/weekend/optionalCoding/palindrome.js
+++ w2/weekend/optionalCoding/palindrome.js
@@ -1,15 +1,15 @@
-function checkPalindrome(inputString) {
+const checkPalindrome = (inputString) => {
   let stringArray =  inputString.split('');
   let reverseArray = stringArray;
-  let arrayLength = stringArray.arrayLength;
-  for( const i = 0 ; i < arrayLength ; i++) {
-      if (stringArray[i] === reverseArray[arrayLength - 1]) {
-          arrayLength --;
-          console.log(reverseArray[arrayLength]);
-          
-       } else {
-           return false;
-       }
-   }
-   return true;
-}
+  let arrayLength = stringArray.length;
+  for (let i = 0; i < arrayLength; i++) {
+    if (stringArray[i] === reverseArray[arrayLength - 1]) {
+      arrayLength --;
+      console.log(reverseArray[arrayLength]);
+        
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
