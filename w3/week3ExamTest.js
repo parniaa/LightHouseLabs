cac56bbcc9468a3d0afbd72d1e55afbe1c71669f
paarmin <parniaa@gmail.com>
2021-01-24 11:02:51 -0500

    durian class

--- w3/week3ExamTest.js
+++ w3/week3ExamTest.js
@@ -52,13 +52,19 @@
 // Given a size in bits convert it to relevant size in B/KB/MB/GB/TB
 
 ///04//////
+
 // const filesize = function(bytes) {
 //   // IMPLEMENT ME
-//   const sizes = ['B', 'kB', 'MB', 'GB', 'TB']
-//   if (bytes === 0) return `0B`;
-//   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)));
-//   if (i === 0) return `${bytes}${sizes[i]}`;
-//   return `${parseFloat((bytes / (1000 ** i)).toFixed(2))}${sizes[i]}`;
+//   const sizes = ['B', 'kB', 'MB', 'GB', 'TB'];
+//   if (bytes === 0) {
+//     return `0B`;
+//   }
+//   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)), 10);
+//   if (i === 0) {
+//     return `${bytes}${sizes[i]}`;
+//   }
+//   let tempVal = (bytes / (Math.pow(1000, i))).toFixed(2);
+//   return `${parseFloat(tempVal)}${sizes[i]}`;
 // };
 
 // console.log(filesize(1250000000));
@@ -66,3 +72,35 @@
 
 
 
+/////depp Array
+// const deepArrayToObject = function(arr) {
+//   // IMPLEMENT ME
+//   let objResult = {};
+//   arr.forEach((value) => {
+//     if (!Array.isArray(value)) {
+//       objResult[value[0]] = value[1];
+//     } else {
+//       value.forEach((v) => {
+//         objResult[v[0]] = v[1];
+//       });
+//     }
+    
+//   });
+//   return objResult;
+// };
+// console.log(deepArrayToObject([['a', 1], ['b', 2], ['c', [['d', 4]]]]);
+
+const deepArrayToObject = function(arr) {
+  // IMPLEMENT ME
+  let objResult = {};
+  arr.forEach((value) => {
+    if (!Array.isArray(value)) {
+      objResult[value[0]] = value[1];
+    } else {
+      deepArrayToObject(value);
+    }
+  });
+  
+  return objResult;
+};
+console.log(deepArrayToObject([['a', 1], ['b', 2], ['c', [['d', 4]]]]));
